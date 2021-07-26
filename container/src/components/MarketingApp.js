9cf02ad6ad3448201588d52f693ec61d85f11f31
DESKTOP-963FGG6\Owlstudio <owlstudio.dsign@gmail.com>
2021-07-26 20:23:55 +0700

    update routing system using memoryHistory

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,29 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
    const ref = useRef();
+   const history = useHistory();
 
    useEffect(() => {
-      mount(ref.current);
+      const { onParentNavigate } = mount(ref.current, {
+         onNavigate: (location) => {
+            // naming aliases of pathname
+            const { pathname: nextPathname } = location;
+            // console.log(nextPathname);
+            const { pathname } = history.location;
+
+            // check if pathname in browser history !== nextPathname param
+            // do some redirection
+            if (pathname !== nextPathname) {
+               history.push(nextPathname);
+            }
+
+         }
+      });
+
+      history.listen(onParentNavigate);
    }, []);
 
    return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-// mount funvtion to start up the app
-const mount = (el) => {
-   ReactDOM.render(<App />, el);
+// mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+   const history = defaultHistory || createMemoryHistory();
+
+   if (onNavigate) {
+      // whenever url/path changing, automatically call onNavigate callback
+      history.listen(onNavigate);
+   }
+
+   ReactDOM.render(<App history={history} />, el);
+
+   return {
+      onParentNavigate(location) {
+         // console.log('Container just navigate')
+         const { pathname: nextPathname } = location;
+         const { pathname } = history.location;
+
+         if (pathname !== nextPathname) {
+            history.push(nextPathname)
+         }
+      }
+   }
 };
 // if we are in development and in isolation
 // call mount immediately
@@ -12,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
    const devRoot = document.querySelector('#_marketing-dev-root');
 
    if (devRoot) {
-      mount(devRoot);
+      mount(devRoot, { defaultHistory: createBrowserHistory() });
    }
 }
 
